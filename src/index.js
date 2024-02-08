46e81a8be05838ab79dbf616aa5a577b82737f31
Orbter <assouline39@gmail.com>
2024-02-08 03:19:30 +0200

    tried to fix photos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import aboutPage from "./pageJs/about";
 import menuPage from "./pageJs/menu.js";
 import homePage from "./pageJs/home.js";
+import leaf from "./img/leaf.svg";
+import pizza from "./img/pizza-guy.svg";
+
 import "./styles/main.scss";
 console.log("poding");
 aboutPage();
