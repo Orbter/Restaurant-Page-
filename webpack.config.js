45fab9415cffcbe0759ab02863b5e2e01f61c07e
Orbter <assouline39@gmail.com>
2024-02-09 18:59:05 +0200

    made the change

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: "/",
+    publicPath: "dist/",
     assetModuleFilename: "[name][ext]",
   },
   module: {
